b617307ecf90bc9834367b7f6e01647ba5dbd9cc
smetak13 <simon.smejkal@seznam.cz>
2024-04-05 13:32:18 +0200

    Fix portrait video generation

--- src/utils/videoio.py
+++ src/utils/videoio.py
@@ -21,51 +21,52 @@ def fill_images(imgs, base_img_path):
     base_orig_width = base_width * 256 // base_height if is_wide_format else 256
     base_orig_height = 256 if is_wide_format else base_height * 256 // base_width
 
-    base_img = base_img.resize((base_orig_width, base_orig_height))  # lower quality of base image to match overlay img
+    base_img = base_img.resize((base_orig_width, base_orig_height)) # lower quality of base image to match overlay img
 
-    base_width = base_width * overlay_height // base_height if is_wide_format else overlay_width
-    base_height = overlay_height if is_wide_format else base_height * overlay_width // base_width
+    base_width = base_width * overlay_height // base_height
+    base_height = overlay_height
 
     base_img = base_img.resize((base_width, base_height))
 
     overlay_images = []
 
-    for frame in tqdm(imgs, "Overlay images gen:"):
+    for index, frame in enumerate(tqdm(imgs, "Overlay images gen:")):
         overlay_img = Image.fromarray(frame).convert("RGBA")
-
         position = ((base_width - overlay_width) // 2, (base_height - overlay_height) // 2)
-
-        mask = np.zeros((overlay_height, overlay_width))
 
         # portrait video
         if not is_wide_format:
             border_top = 20
             border_bottom = 120
+            mask = np.zeros((overlay_height, overlay_width))
             for y in range(overlay_height):
                 for x in range(overlay_width):
                     dist_y_top = y
                     dist_y_bottom = overlay_height - y - 1
 
+                    factor_top = 1 if index < 10 else (dist_y_top / border_top)
+                    factor_bottom = 1 if index < 10 else (dist_y_bottom / border_bottom)
                     if dist_y_top > border_top and dist_y_bottom > border_bottom:
                         mask[y][x] = 255
                     elif dist_y_top <= border_top:
-                        mask[y][x] = 255 * (dist_y_top / border_top)
+                        mask[y][x] = 255 * factor_top
                     else:
-                        mask[y][x] = 255 * (dist_y_bottom / border_bottom)
+                        mask[y][x] = 255 * factor_bottom
         # wide video
         else:
             border = 200
+            mask = np.zeros((overlay_height, overlay_width))
             for y in range(overlay_height):
                 for x in range(overlay_width):
                     dist_x = min(x, overlay_width - x)
 
+                    factor = dist_x / border
                     if dist_x > border:
                         mask[y][x] = 255
                     else:
-                        mask[y][x] = 255 * (dist_x / border)
+                        mask[y][x] = 255 * factor
 
         mask = Image.fromarray(np.uint8(mask), 'L')
-
         base_img.paste(overlay_img, position, mask)
 
         open_cv_image = np.array(base_img)
